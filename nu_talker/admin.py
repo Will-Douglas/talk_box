cb7bf28b029baa17859405e7c1442726f94c5a40
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-11-11 04:22:15 -0500

    Set up my models for NuTalker and registered it to my admin.

--- nu_talker/admin.py
+++ nu_talker/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from nu_talker.models import NuTalker
 
 # Register your models here.
+admin.site.register(NuTalker)
--- nu_talker/models.py
+++ nu_talker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ForeignKey
 # Create your models here.
+
+class NuTalker(AbstractUser):
+    name = models.CharField(max_length=150)
+    age = models.IntegerField(null=True)
+    hometown = models.CharField(max_length=150)
+    connection = models.ManyToManyField("self", symmetrical=False)
