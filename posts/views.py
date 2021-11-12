85a69f6b5618721c748d64f9e8fd9bf371c0bebd
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-11-11 23:30:51 -0500

    Added the chatpage.html file into my code so far.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from posts.models import TalkTalk
+
 # Create your views here.
+def talk_view(request, post_id:int):
+    speaking = TalkTalk.objects.get(id=post_id)
+    return render(request, 'chatpage.html', {:})
