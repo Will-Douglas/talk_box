1b5fe1dae820b1f157d545b0608e7b0c4f710f2e
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-11-14 03:29:11 -0500

    I created the Login View with this entry. Next I'll create the signup form.html.

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+
+from nu_talker.models import NuTalker
+from posts.models import TalkTalk
+
+from authentication.forms import LogInForm, SignUpForm
+
 
 # Create your views here.
+def LoginView(request):
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            talker = authenticate(
+                username=data['username'], password=data['password'])
+            if talker:
+                login(request, talker)
+                return HttpResponseRedirect(reverse("Homepage"))
+
+    form = LogInForm()
+    return render(request, 'signupform.html', {'form':form})
--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.db.models.fields.related import ForeignKey
 from nu_talker.models import NuTalker
+
+
 # Create your models here.
+
 class TalkTalk(models.Model):
     body = models.TextField(max_length=300)
     talk_stated = models.DateTimeField(default=timezone.now)
