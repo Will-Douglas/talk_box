d03a588df817492a271328ce3c2dd2d58664b93c
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-11-17 17:25:20 -0500

    Ran into an error with the submittion of the sign up form.

--- authentication/views.py
+++ authentication/views.py
@@ -31,12 +31,12 @@ def TalkLogOut(request):
 def SignUpView(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             data = form.cleaned_data
             nu_talker = NuTalker.objects.create_user(username=data['username'], password=data['password'],
                                                         age=data['age'], hometown=data['hometown'], bio=data['bio'])
         user = authenticate(
-            request, username=data['username'], password=data['data'])
+            request, username=data['username'], password=data['password'])
         if user:
             login(request, user)
             return HttpResponseRedirect(request.GET.get('next', reverse("Login")))
--- nu_talker/views.py
+++ nu_talker/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
+from nu_talker.models import NuTalker
 from posts.models import TalkTalk
 
 #Create your views here.
 
 def index(request):
+
+    connection_list = []
+
     all_posts = TalkTalk.objects.all()
-    
+   
+
     return render(request, "index.html", {"all_posts":all_posts})
+
+def profile(request, user_id: int):
+    tha_talker = NuTalker.objects.get(id=user_id)
+    all_posts = TalkTalk.objects.filter(talker_user=user_id)
+    return render(request, "talkerprofile.html", {"all_posts": all_posts, "tha_talker": tha_talker})
