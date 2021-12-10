36b00b2de3c4ef12defd9d50b08b719b45bd6507
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-12-09 23:01:54 -0500

    Added the connection view for those who wish to be connected to their friends profile.

--- nu_talker/views.py
+++ nu_talker/views.py
@@ -6,7 +6,7 @@ from nu_talker.models import NuTalker
 from posts.models import TalkTalk
 
 #Create your views here.
-
+@login_required
 def index(request):
 
     connection_list = []
@@ -20,3 +20,11 @@ def profile(request, user_id: int):
     tha_talker = NuTalker.objects.get(id=user_id)
     all_posts = TalkTalk.objects.filter(spokesmen = user_id)
     return render(request, "talkerprofile.html", {"all_posts": all_posts, "tha_talker": tha_talker})
+
+def ConnectionView(request, user_id: int):
+    connected = NuTalker.objects.get(id=user_id)
+    if request.user.is_authenticated:
+        auth_user = NuTalker.objects.get(id=request.user.id)
+        auth_user.connected.add(connected)
+        auth_user.save()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
