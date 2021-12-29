b776a70ee94d19cf21f53e913a1bf83da6bb0f61
Willie Douglas Jr <w.douglas.jr6119@gmail.com>
2021-12-29 01:32:27 -0500

    I was able to get the disconnection view to work properly, when you disconnect from someone you are now removed from their list.

--- nu_talker/views.py
+++ nu_talker/views.py
@@ -35,10 +35,10 @@ def ConnectionView(request, user_id: int):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def DisconnectionView(request, user_id: int):
-    connected = NuTalker.objects.get(id=user_id)
+    connection = NuTalker.objects.get(id=user_id)
     if request.user.is_authenticated:
         auth_user = NuTalker.objects.get(id=request.user.id)
-        auth_user.connected.remove(connected)
+        auth_user.connection.remove(connection)
         auth_user.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
